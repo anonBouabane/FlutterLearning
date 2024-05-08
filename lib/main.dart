870af4e5f270db9fbffaa2a571c6cbbbdf4ec279
Aiy <anon@gmail.com>
2024-05-08 18:27:56 +0700

    aiy

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> baner = [
+    "assets/images/picture01.png",
+    "assets/images/picture.png"
+    ,"assets/images/picture.png"
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -62,6 +73,8 @@ class MyApp extends StatelessWidget {
                     width: MediaQuery.of(context).size.width / 2,
                     child: TextFormField(
                       decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 15, vertical: 10),
                           hintText: "search...",
                           border: InputBorder.none,
                           suffixIcon: Icon(Icons.search)),
@@ -72,7 +85,9 @@ class MyApp extends StatelessWidget {
                     child: Container(
                       height: 50,
                       width: 50,
-                      decoration:   BoxDecoration(color: Colors.amber,borderRadius: BorderRadius.circular(10)),
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(10)),
                       child: const Icon(
                         Icons.apps,
                         color: Colors.white,
@@ -82,6 +97,25 @@ class MyApp extends StatelessWidget {
                 ],
               ),
             ),
+            CarouselSlider(
+              options: CarouselOptions(
+                  height: 200.0,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  viewportFraction: 1,
+                  autoPlayAnimationDuration: const Duration(milliseconds: 300)),
+              items:  baner.map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: const BoxDecoration(color: Colors.amber),
+                        child: Image.asset(i,fit: BoxFit.cover,));
+                  },
+                );
+              }).toList(),
+            )
           ],
         )),
       ),
